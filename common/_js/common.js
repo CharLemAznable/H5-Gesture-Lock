2ca5a1b483ee4a3a5bc46759bba68b6dcf5e1fbb
CharLemAznable <545541819@qq.com>
2016-03-23 20:12:45 +0800

    Image optimized.

--- common/_js/common.js
+++ common/_js/common.js
@@ -11,4 +11,17 @@
               + imgSrcPath.substring(suffixPos);
     return img;
   }
+
+  Common.urlParam = function(name) {
+    var value = "", found = false;
+    if (window.location.search.indexOf("?") == 0 && window.location.search.indexOf("=") > 1) {
+        var arr = unescape(window.location.search).substring(1, window.location.search.length).split("&");
+        for (var i = 0; i < arr.length && !found; i++) {
+          arr[i].indexOf("=") > 0 &&
+          arr[i].split("=")[0].toLowerCase() == name.toLowerCase() &&
+          (value = arr[i].split("=")[1], found = true);
+        }
+    }
+    return value == "" && (value = null), value
+  }
 })();
